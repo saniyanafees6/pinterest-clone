cac4d6ec9924d7179379c5b26d56c879ba35ff2d
Saniya Nafees <saniyanafees6@gmail.com>
2022-01-30 18:55:53 -0700

    fix: gitignore fix

---
+++ pinterest-frontend/src/components/MasonryLayout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Masonry from 'react-masonry-css';
+import Pin from './Pin';
+
+const breakpointColumnsObj = {
+  default: 3,
+  3000: 3,
+  2000: 3,
+  1200: 3,
+  1000: 2,
+  500: 1,
+};
+
+const MasonryLayout = ({ pins }) => (
+  <Masonry
+    className="flex animate-slide-fwd"
+    breakpointCols={breakpointColumnsObj}
+  >
+    {pins?.map((pin) => (
+      <Pin key={pin._id} pin={pin} className="w-max" />
+    ))}
+  </Masonry>
+);
+
+export default MasonryLayout;
